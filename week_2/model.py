41476551882fa41650316453bcf580ec9388cb2c
Viktor Valastin <viktorko99@gmail.com>
2019-10-02 11:01:18 +0200

    WIP: added stochastic_gradient

--- week_2/model.py
+++ week_2/model.py
@@ -97,4 +97,18 @@ class LinearRegressionModel:
         yp = self.predict(xs)
         length = len(xs)
 
-        return np.sum(self.xyz(yp, ys)) / length  # FIXME: 2.9.4
+        return np.sum(self.xyz(yp, ys)) / length
+
+    def stochastic_gradient_descent(self, xs, ys, num_epochs=10, batch_size=2):
+    """
+    Performs a stochastic gradient descent training over the data xs, ys.
+
+    :param num_epochs: Number of epochs for the training.
+    :param batch_size: Number of samples in batches.
+    :return: None
+    """
+    for _ in range(num_epochs):
+        ...
+        for _ in range(...):
+            self.step(..., ...)
+    # FIXME: 2.13
