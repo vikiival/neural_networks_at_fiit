fixed stochastic gradient

week_2/lrm.py
@@ -2,21 +2,21 @@ import numpy as np
 
 from model import LinearRegressionModel
 
+learning_rate = 0.03
+num_epochs = 10
+batch_size = 1
+
 m = LinearRegressionModel(
-            input_dim=1,
-            w=np.array([0.5]),
-            b=0.2)
+        input_dim=1,
+        learning_rate=learning_rate,
+        w=[2],
+        b=-2)
 
 
 d = (
-            np.array([
-                [1.2],
-                [1.5],
-                [1.3],
-                [1.7]
-            ]),
-            np.array([0.2, 0.4, 0.5, 3.8])
-        )
+        np.random.rand(9, 3),
+        np.array([i for i in range(9)])
+    )
 
 
 def test_predict():

week_2/model.py
@@ -100,15 +100,18 @@ class LinearRegressionModel:
         return np.sum(self.xyz(yp, ys)) / length
 
     def stochastic_gradient_descent(self, xs, ys, num_epochs=10, batch_size=2):
-    """
-    Performs a stochastic gradient descent training over the data xs, ys.
+    # """
+    # Performs a stochastic gradient descent training over the data xs, ys.
 
-    :param num_epochs: Number of epochs for the training.
-    :param batch_size: Number of samples in batches.
-    :return: None
-    """
-    for _ in range(num_epochs):
-        ...
-        for _ in range(...):
-            self.step(..., ...)
-    # FIXME: 2.13
+    # :param num_epochs: Number of epochs for the training.
+    # :param batch_size: Number of samples in batches.
+    # :return: None
+    # """
+        for epoch in range(num_epochs):
+            rand = np.arange(len(xs))
+            np.random.shuffle(rand)
+            xs = xs[rand]
+            ys = ys[rand]
+
+            for i in range(0,len(ys), batch_size):
+                self.step(xs[i:i+batch_size ], ys[i:i+batch_size ])