b1040a6a4360831326b9a32dbca42b97bae4bb5f
Viktor Valastin <viktorko99@gmail.com>
2019-10-16 12:23:35 +0200

    added week 4

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 open /Applications/Docker.app;
-docker run -u $(id -u):$(id -g) -p 8888:8888 -v $(pwd):/labs -it mpikuliak/nsiete
+# docker run -u $(id -u):$(id -g) -p 8888:8888 -v $(pwd):/labs -it mpikuliak/nsiete
+docker run -u $(id -u):$(id -g) -p 8888:8888 -p 6006:6006 -v $(pwd):/labs -it mpikuliak/nsiete
